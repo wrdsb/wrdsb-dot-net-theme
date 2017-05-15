aa6410b1adda62d961a0f9e97951c3c9a076438e
dtwardus <dorian_twardus@wrdsb.on.ca>
2017-05-15 14:25:38 -0400

    Updated Login templates.Authorization can occur through tontrol table, AD, etc. Removed Board Users table.

---
+++ DotNetThemeMVC/DotNetThemeMVC/Models/UserRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNetThemeMVC.Models
+{
+    public class UserRoleViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public IList<string> userRoles { get; set; }
+        public List<string> allRoles { get; set; }
+    }
+}
---
+++ DotNetThemeMVC/DotNetThemeMVC/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotNetThemeMVC.Models
+{
+    public class UserViewModel
+    {
+        public IPagedList<UserRoleViewModel> Users { get; set; }
+        public string searchString { get; set; }
+        public string roleFilter { get; set; }
+        public int pageSize { get; set; }
+        public int page { get; set; }
+        public string sortOrder { get; set; }
+    }
+}
