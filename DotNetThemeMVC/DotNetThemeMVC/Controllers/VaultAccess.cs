7cb7faf2e4bb3258589e609cf4a4d85163b109fe
dtwardus <dorian_twardus@wrdsb.on.ca>
2017-08-16 14:57:18 -0400

    Azure Key Vault Test Code

---
+++ DotNetThemeMVC/DotNetThemeMVC/Controllers/VaultAccess.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace DotNetThemeMVC.Controllers
+{
+    public class VaultAccess
+    {
+        //this is an optional property to hold the secret after it is retrieved
+        public static string EncryptSecret { get; set; }
+        public static ClientAssertionCertificate AssertionCert { get; set; }
+
+        //the method that will be provided to the KeyVaultClient
+        public static async Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(WebConfigurationManager.AppSettings["ClientId"],
+                        WebConfigurationManager.AppSettings["ClientSecret"]);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+
+            return result.AccessToken;
+        }
+
+        public static void GetCert()
+        {
+            var clientAssertionCertPfx = CertificateHelper.FindCertificateByThumbprint(WebConfigurationManager.AppSettings["thumbprint"]);
+            AssertionCert = new ClientAssertionCertificate(WebConfigurationManager.AppSettings["ClientId"], clientAssertionCertPfx);
+        }
+    }
+}
--- DotNetThemeMVC/DotNetThemeMVC/Global.asax.cs
+++ DotNetThemeMVC/DotNetThemeMVC/Global.asax.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.Azure.KeyVault;
+using System.Web.Configuration;
+using DotNetThemeMVC.Controllers;
 
 namespace DotNetThemeMVC
 {
@@ -16,6 +19,16 @@ namespace DotNetThemeMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // I put my GetToken method in a Utils class. Change for wherever you placed your method.
+            VaultAccess.GetCert();
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(VaultAccess.GetAccessToken));
+
+            var sec = kv.GetSecretAsync(WebConfigurationManager.AppSettings["SecretUri"]);
+
+            //I put a variable in a Utils class to hold the secret for general  application use.
+            VaultAccess.EncryptSecret = sec.Result.Value;
+            System.Diagnostics.Debug.WriteLine(sec.Result.Value.ToString());
         }
     }
 }
